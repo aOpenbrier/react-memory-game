577e4a01dfff37f7545409a01372ebd055b81837
aOpenbrier <aopenbrier@gmail.com>
2019-01-08 01:59:43 -0800

    routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Game from './components/Game'
+import Home from './components/Home'
+import LeaderBoard from './components/LeaderBoard'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Router>
+        <div style={{background: 'url(./assets/images/bones.jpg) fixed center /cover', minHeight: '100vh'}}>
+          <Route exact path='/' component={Home}/>
+          <Route path='/Game' component={Game} />
+          <Route path='./LeaderBoard' component={LeaderBoard} />
+          </div>
+        </Router>
     );
   }
 }
