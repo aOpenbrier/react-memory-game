cd0ce99d9db78f59b838e37c20379aae8dfbdc03
aOpenbrier <aopenbrier@gmail.com>
2019-01-10 02:39:14 -0800

    update routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Game from './components/Game'
 import Home from './components/Home'
-import LeaderBoard from './components/LeaderBoard'
 
 import './App.css';
 
@@ -13,7 +12,6 @@ class App extends Component {
         <div style={{background: 'url(./assets/images/bones.jpg) fixed center /cover', minHeight: '100vh'}}>
           <Route exact path='/' component={Home}/>
           <Route path='/Game' component={Game} />
-          <Route path='./LeaderBoard' component={LeaderBoard} />
           </div>
         </Router>
     );
