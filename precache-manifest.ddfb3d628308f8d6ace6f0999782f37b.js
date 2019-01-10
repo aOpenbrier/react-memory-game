81139cbf51378e8847759d4d874b347bfda0c3c6
aOpenbrier <aopenbrier@gmail.com>
2019-01-10 02:44:24 -0800

    Updates

---
+++ precache-manifest.ddfb3d628308f8d6ace6f0999782f37b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aOpenbrier/react-memory-game.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8f91f9e6dcbc2b64aa30",
+    "url": "/aOpenbrier/react-memory-game.git/static/js/runtime~main.8f91f9e6.js"
+  },
+  {
+    "revision": "eae9a71cd95784443b13",
+    "url": "/aOpenbrier/react-memory-game.git/static/js/main.eae9a71c.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/aOpenbrier/react-memory-game.git/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "eae9a71cd95784443b13",
+    "url": "/aOpenbrier/react-memory-game.git/static/css/main.196b046f.chunk.css"
+  },
+  {
+    "revision": "5827cba45e5b3f465695f6c83097d81b",
+    "url": "/aOpenbrier/react-memory-game.git/index.html"
+  }
+];
